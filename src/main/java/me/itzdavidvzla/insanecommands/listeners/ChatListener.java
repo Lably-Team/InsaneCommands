c71fa7089adeced0f51d7f8dcda912d660920f13
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-21 07:51:42 -0500

    Update listener

--- src/main/java/me/itzdavidvzla/insanecommands/listeners/ChatListener.java
+++ src/main/java/me/itzdavidvzla/insanecommands/listeners/ChatListener.java
@@ -6,18 +6,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
 import me.itzdavidvzla.insanecommands.InsaneCommands;
 
 public class ChatListener implements Listener{
 
-	private InsaneCommands plugin;
+	private final InsaneCommands plugin;
 	public ChatListener(InsaneCommands plugin) {
 		this.plugin = plugin;
 	}
 	
 	@EventHandler
-	public void ChatListener(AsyncPlayerChatEvent event){
+	public void setChatFormat(AsyncPlayerChatEvent event){
 		Player player = event.getPlayer();
 		FileConfiguration messages = plugin.getMessages();
 		event.setFormat(ChatColor.translateAlternateColorCodes('&', "&c"+player.getDisplayName()+ " &8>&7 " +event.getMessage()+ ""));
