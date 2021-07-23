41620bc11d609e515177457c4aeeb880166d9540
ItzDavidVZLA_ <itzdavldvzla@gmail.com>
2021-07-23 02:31:19 -0500

    Update InsaneCommands.java

--- src/main/java/me/itzdavidvzla/insanecommands/InsaneCommands.java
+++ src/main/java/me/itzdavidvzla/insanecommands/InsaneCommands.java
@@ -1,17 +1,21 @@
 package me.itzdavidvzla.insanecommands;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public final class InsaneCommands extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&cInsaneCommands&8] &7Plugin &aenabled"));
         PluginCore pluginCore = new PluginCore(this);
 
     }
 
     @Override
     public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&cInsaneCommands&8] &7Plugin &cdisabled"));
         // Plugin shutdown logic
     }
 }
