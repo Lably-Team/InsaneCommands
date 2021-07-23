659a53905a18402055d6e22185bdaa67c41881ef
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-23 08:52:45 -0500

    Add static method

---
+++ src/main/java/me/itzdavidvzla/insanecommands/utils/TextColor.java
@@ -0,0 +1,11 @@
+package me.itzdavidvzla.insanecommands.utils;
+
+import org.bukkit.ChatColor;
+
+public class TextColor {
+
+    public static String colorized(String path) {
+        return ChatColor.translateAlternateColorCodes('&', path);
+    }
+
+}
