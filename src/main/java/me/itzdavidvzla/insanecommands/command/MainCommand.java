29c141b06c243cffb36d614de7ca86c3fa01c775
ItzDavidVZLA_ <itzdavldvzla@gmail.com>
2021-07-23 12:13:58 -0500

    Update MainCommand.java

--- src/main/java/me/itzdavidvzla/insanecommands/command/MainCommand.java
+++ src/main/java/me/itzdavidvzla/insanecommands/command/MainCommand.java
@@ -1,21 +1,28 @@
 package me.itzdavidvzla.insanecommands.command;
 
 import me.itzdavidvzla.insanecommands.PluginCore;
+import me.itzdavidvzla.insanecommands.utils.TextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class MainCommand implements CommandExecutor{
+public class MainCommand implements CommandExecutor {
 
     private PluginCore pluginCore;
 
-    public MainCommand(PluginCore pluginCore){
+    public MainCommand(PluginCore pluginCore) {
         this.pluginCore = pluginCore;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        Bukkit.getConsoleSender().sendMessage(TextColor.colorized("Messages.help_lore"));
         return true;
+
+        }
     }
-}
+
+
+
