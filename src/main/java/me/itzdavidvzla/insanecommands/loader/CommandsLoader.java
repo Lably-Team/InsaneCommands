7d597f62b1dc8b2c551a6d9544df748462c34cd4
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-23 23:43:44 -0500

    Add instance of feed command

--- src/main/java/me/itzdavidvzla/insanecommands/loader/CommandsLoader.java
+++ src/main/java/me/itzdavidvzla/insanecommands/loader/CommandsLoader.java
@@ -2,13 +2,14 @@ package me.itzdavidvzla.insanecommands.loader;
 
 import me.itzdavidvzla.insanecommands.PluginCore;
 import me.itzdavidvzla.insanecommands.api.Loader;
+import me.itzdavidvzla.insanecommands.command.CommandFeed;
 import me.itzdavidvzla.insanecommands.command.MainCommand;
 import me.itzdavidvzla.insanecommands.command.builder.ExecutorBuilder;
 import org.bukkit.Bukkit;
 
 public class CommandsLoader implements Loader {
 
-    private PluginCore pluginCore;
+    private final PluginCore pluginCore;
 
     public CommandsLoader(PluginCore pluginCore){
         this.pluginCore = pluginCore;
@@ -17,7 +18,9 @@ public class CommandsLoader implements Loader {
     @Override
     public void load() {
         registerCommands(
-                new ExecutorBuilder("defaultcommand", new MainCommand(pluginCore)));
+                new ExecutorBuilder("ic", new MainCommand(pluginCore)),
+                new ExecutorBuilder("feed", new CommandFeed(pluginCore))
+        );
     }
 
     public void registerCommands(ExecutorBuilder... executorBuilders){
